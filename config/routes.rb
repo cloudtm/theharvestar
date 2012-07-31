14380aaa1ef5eece94a3f38bd9f2f970c10ee64b
Marco Zaratti <marco.zaratti@gmail.com>
2012-07-31 20:05:17 +0200

    Switched devise authentication names

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Theharvestar::Application.routes.draw do
 
   devise_for :users, :skip => [:sessions]
     as :user do
-      get 'signup' => 'devise/sessions#new', :as => :new_user_session
-      post 'signin' => 'devise/sessions#create', :as => :user_session
+      get 'signin' => 'devise/sessions#new', :as => :new_user_session
+      post 'signup' => 'devise/sessions#create', :as => :user_session
       get 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
